46e6a72f9d7d7a6d364e01b01646f52015764aee
jurinakayama <jurin2023@gmail.com>
2026-02-17 21:31:29 -0800

    modified:   src/data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+file_path = 'data/online_retail_II.xlsx'
+df1 = pd.read_excel(file_path, sheet_name='Year 2009-2010')
+df2 = pd.read_excel(file_path, sheet_name='Year 2010-2011')
+df1.to_csv('data/online_retail_2009_2010.csv', index=False)
+df2.to_csv('data/online_retail_2010_2011.csv', index=False)
+
+
+df1 = pd.read_csv('data/online_retail_2009_2010.csv')
+df2 = pd.read_csv('data/online_retail_2010_2011.csv')
+
+df = pd.concat([df1, df2])
+print(df.head())
+
